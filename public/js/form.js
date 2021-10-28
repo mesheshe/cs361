4f20391c8b1cebc513155e80b6254408d1520ede
mesheshe <alias49@live.com>
2021-10-27 17:50:32 -0700

    Fixed some ui elements

--- public/js/form.js
+++ public/js/form.js
@@ -1,16 +1,25 @@
 function openTab(event, opt){
-    var i, tabcontent, tablinks;
-
+    var i, tabcontent, tabs, starts, ends;
     tabcontent = document.getElementsByClassName('tabcontent');
-    for (i = 0; i < tabcontent.length; i++){
+    tabs = document.getElementsByClassName("nav-link");
+    if (event.currentTarget.className == "nav-link active"){return;}
+    if (parseInt(opt) <= 3){
+        starts = 0;
+        ends = 3;
+    }else{
+        starts = 3;
+        ends = tabcontent.length;
+    }
+    console.log(starts, ends);
+    for (i = starts; i < ends; i++){
         tabcontent[i].style.display = "none";
     }
     tabcontent[parseInt(opt)-1].style.display = "block";  
-    var tabs = document.getElementsByClassName("nav-link");
-    for (i = 0; i< tabs.length; i++){
-        if (tabs[i].getAttribute("class") == "nav-link active"){
-            tabs[i].setAttribute("class", "nav-link")
-        }
+    // We are adding plus 3 because the header also has elements with 
+    // attribute nav-link, therefore need to offset to fix the issue.
+    for (i = starts + 3; i< ends + 3; i++){
+        if (tabs[i].getAttribute("class") == "nav-link active")
+            tabs[i].setAttribute("class", "nav-link");
     }
     event.currentTarget.className += " active";
 }
