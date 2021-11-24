d74da8a4ef192849fc02d44bee25200515331bd7
mesheshe <alias49@live.com>
2021-11-23 20:16:41 -0800

    Adding some stuff

--- public/js/form.js
+++ public/js/form.js
@@ -45,7 +45,7 @@ sub.addEventListener("click", function(event){
         req.addEventListener("load", function(){
             if (req.status >= 200 && req.status < 400){
                 buildCoord(JSON.parse(req.responseText));
-                populatePark();
+                populatePark(); test();
             }else{alert("Error: Invalid Submission")}
         })
         req.send(JSON.stringify(payload));
@@ -62,7 +62,8 @@ async function populatePark(){
     checkBoundary("hello")
     for (const key in parkData){
         if (checkBoundary([parkData[key].Longitude, parkData[key].Latitude])){
-            createCard(key, parkData, container)
+            let content = `${key} is located at ${parkData[key].State} at longitude ${parkData[key].Longitude} and latitude ${parkData[key].Latitude}. <br>This park was established in ${parkData[key].Date_Established} covering an area of ${parkData[key].Area} km^2, with a yearly visitors <br>count of ${parkData[key].Visitors}. For more info, visit <a target="_blank" href ="${parkData[key].Website}">here</a>.`;
+            createCard(key, parkData, container, content)
         }
     }
 }
@@ -78,7 +79,7 @@ function createCardElements(){
     return obj 
 }
 
-function fillOutCardElements(key, parkData, obj){
+function fillOutCardElements(key, parkData, obj, content){
     obj.header.textContent = key;
     obj.button.setAttribute('class', 'btn btn-primary');
     obj.button.textContent = 'Place Marker';
@@ -86,19 +87,16 @@ function fillOutCardElements(key, parkData, obj){
         addMarkerFromLonLatArr([[parkData[key].Longitude, parkData[key].Latitude]], [key])
         document.getElementById('my-map').scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
     })
-    obj.content.innerHTML = 
-    `${key} is located at ${parkData[key].State} at longitude ${parkData[key].Longitude} and latitude ${parkData[key].Latitude}. <br>`+
-    `This park was established in ${parkData[key].Date_Established} covering an area of ${parkData[key].Area} km^2, with a yearly visitors <br>`+
-    `count of ${parkData[key].Visitors}. For more info, visit <a target="_blank" href ="${parkData[key].Website}">here</a>.`;
+    obj.content.innerHTML = content;
     obj.card.setAttribute('class', 'card');
     obj.cardBody.setAttribute('class', 'card-body');
     obj.tabContent.setAttribute('class','tabcontent');
     return obj
 }
 
-function createCard(key, parkData, container){
+function createCard(key, parkData, container, content){
     let obj = createCardElements()
-    let newObj = fillOutCardElements(key, parkData, obj)
+    let newObj = fillOutCardElements(key, parkData, obj, content)
     newObj.tabContent.appendChild(newObj.header);
     newObj.tabContent.appendChild(newObj.content);
     newObj.tabContent.appendChild(newObj.button);
@@ -167,7 +165,6 @@ function buildCoord(data){
 function addMarkerFromLonLatArr(arr, arr2 = false){
     // I kept it in terms of i so if you have another array that 
     // has information about said marker you can still reference it
-     
     for (let i = 0; i < arr.length; i++){
         let icon = document.createElement('div');
         icon.classList.add("icon");
@@ -177,10 +174,7 @@ function addMarkerFromLonLatArr(arr, arr2 = false){
             anchor: 'bottom',
             offset: [0, -64] // height - shadow
           });
-        if (arr2){
-            iconPopup.setText(`${arr2[i]}`);   
-        }
-
+        if (arr2){iconPopup.setText(`${arr2[i]}`);}
         let iconMarker = new maplibregl.Marker(icon, {
             anchor: 'bottom',
             offset: [0, 6]
@@ -188,20 +182,41 @@ function addMarkerFromLonLatArr(arr, arr2 = false){
         .setLngLat(arr[i])
         .setPopup(iconPopup)
         .addTo(map);
-
-        //map.setCenter(arr[i])
-        //icon.onclick = (event) => {
-        //    // you can add custom logic here. For example, modify popup.
-        //    iconPopup.setHTML(`<p>${event.target.getAttribute('id')}</p>`);
-        //    event.stopPropagation();
-        //}
-
-          icon.onmouseenter = () => iconMarker.togglePopup(); // show/hide popup on mouse hover
-          icon.onmouseleave = () => iconMarker.togglePopup();
-
-          
-          bounds.extend(arr[i]);
+        icon.onmouseenter = () => iconMarker.togglePopup(); // show/hide popup on mouse hover
+        icon.onmouseleave = () => iconMarker.togglePopup();
+        bounds.extend(arr[i]);
     }
     map.fitBounds(bounds,{padding: {top:100, bottom:50, left:25, right:25}});
 }
 
+async function test(){
+    let container = document.getElementsByClassName('containers')[4];
+    //circle:lon,lat,radiusMeters
+    const response = await fetch('https://api.geoapify.com/v2/places?categories=accommodation.hotel&filter=circle:'+ route[0][0] +','+ route[0][1] +',5000&limit=2&apiKey=b1f46c812f0f4a7485e46c797622ab4a');
+    const data1 = await response.json();
+    let data = data1.features;
+    for (let i = 0; i < data.length; i++){
+        if (data[i].properties.name !== undefined){
+            // Fixing a bug
+            let key = data[i].properties.name;
+            let obj  = {}; obj[key] = {Longitude:data[i].properties.lon,Latitude:data[i].properties.lat}
+            let content =  `${key} is located at longitude ${obj[key].Longitude} and latitude ${obj[key].Latitude}`;
+            createCard(data[i].properties.name,obj,container,content)
+            console.log();
+        }
+    }
+    //
+}
+
+
+function measure(lon1, lat1, lon2, lat2){  // generally used geo measurement function
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d * 1000; // meters
+}
